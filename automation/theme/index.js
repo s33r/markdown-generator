ead23e87eb399d15981529da92e2e00106e1c94f
Aaron Willows <aaron@aaronwillows.com>
2019-02-17 22:44:04 -0800

    Updated docs with material design

--- automation/theme/index.js
+++ automation/theme/index.js
@@ -4,6 +4,8 @@ module.exports = {
     templateLocations: {
         page: path.resolve(__dirname, './page.tpl.html'),
         table: path.resolve(__dirname, './table.tpl.html'),
+        paragraph: path.resolve(__dirname, './paragraph.tpl.html'),
+        heading: path.resolve(__dirname, './heading.tpl.html'),
     },
     themeFiles: [
         path.resolve(__dirname, './index.css'),
